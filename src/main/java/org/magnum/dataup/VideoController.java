77d60274299cd7a7b0c0d0503a06f79cfd4544d8
Anh Luong Latitude E6430 <luonanh@gmail.com>
2014-08-04 21:54:40 -0500

    Rename AnEmptyController to VideoController.

---
+++ src/main/java/org/magnum/dataup/VideoController.java
@@ -0,0 +1,99 @@
+/*
+ * 
+ * Copyright 2014 Jules White
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+package org.magnum.dataup;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.magnum.dataup.model.Video;
+import org.magnum.dataup.model.VideoStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import retrofit.client.Response;
+import retrofit.mime.TypedFile;
+
+@Controller
+public class VideoController {
+
+	/**
+	 * You will need to create one or more Spring controllers to fulfill the
+	 * requirements of the assignment. If you use this file, please rename it
+	 * to something other than "AnEmptyController"
+	 * 
+	 * 
+		 ________  ________  ________  ________          ___       ___  ___  ________  ___  __       
+		|\   ____\|\   __  \|\   __  \|\   ___ \        |\  \     |\  \|\  \|\   ____\|\  \|\  \     
+		\ \  \___|\ \  \|\  \ \  \|\  \ \  \_|\ \       \ \  \    \ \  \\\  \ \  \___|\ \  \/  /|_   
+		 \ \  \  __\ \  \\\  \ \  \\\  \ \  \ \\ \       \ \  \    \ \  \\\  \ \  \    \ \   ___  \  
+		  \ \  \|\  \ \  \\\  \ \  \\\  \ \  \_\\ \       \ \  \____\ \  \\\  \ \  \____\ \  \\ \  \ 
+		   \ \_______\ \_______\ \_______\ \_______\       \ \_______\ \_______\ \_______\ \__\\ \__\
+		    \|_______|\|_______|\|_______|\|_______|        \|_______|\|_______|\|_______|\|__| \|__|
+                                                                                                                                                                                                                                                                        
+	 * 
+	 */
+	
+	private static final AtomicLong currentId = new AtomicLong(0L);
+	private Map<Long,Video> videos = new HashMap<Long, Video>();
+
+	@RequestMapping(value=VideoSvcApi.VIDEO_SVC_PATH, method=RequestMethod.GET)
+	public @ResponseBody Collection<Video> getVideoList(){
+		return videos.values();
+	}
+
+	@RequestMapping(value=VideoSvcApi.VIDEO_SVC_PATH, method=RequestMethod.POST)
+	public @ResponseBody Video addVideo(@RequestBody Video v){
+		Video video = Video.create().withContentType(v.getContentType())
+				.withDuration(v.getDuration()).withSubject(v.getSubject())
+				.withTitle(v.getTitle()).build();
+		return save(video);
+	}	
+	
+	@RequestMapping(value=VideoSvcApi.VIDEO_DATA_PATH, method=RequestMethod.POST)
+	public VideoStatus setVideoData(
+			@RequestParam(VideoSvcApi.ID_PARAMETER) long id, 
+			@RequestParam(VideoSvcApi.DATA_PARAMETER) TypedFile videoData) {
+		return new VideoStatus(VideoStatus.VideoState.READY); // ALDBG TODO
+	}
+	
+	@RequestMapping(value=VideoSvcApi.VIDEO_DATA_PATH, method=RequestMethod.GET)
+	Response getData(@RequestParam(VideoSvcApi.ID_PARAMETER) long id) {
+		return null; // ALDBG TODO
+	}
+	
+  	public Video save(Video entity) {
+		checkAndSetId(entity);
+		videos.put(entity.getId(), entity);
+		return entity;
+	}
+
+	private void checkAndSetId(Video entity) {
+		if(entity.getId() == 0){
+			entity.setId(currentId.incrementAndGet());
+		}
+	}
+
+}
