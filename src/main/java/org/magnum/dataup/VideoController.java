b129be7a7ae9fbf15fd81322cf7e75f97ad35686
Anh Luong Latitude E6430 <luonanh@gmail.com>
2014-08-06 17:02:19 -0500

    Finish all the tests by following Simon's guide in https://class.coursera.org/mobilecloud-001/forum/thread?thread_id=545

--- src/main/java/org/magnum/dataup/VideoController.java
+++ src/main/java/org/magnum/dataup/VideoController.java
@@ -17,16 +17,19 @@
  */
 package org.magnum.dataup;
 
+import java.io.IOException;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.magnum.dataup.model.Video;
 import org.magnum.dataup.model.VideoStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -34,9 +37,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
-
-import retrofit.client.Response;
-import retrofit.mime.TypedFile;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class VideoController {
@@ -60,6 +61,7 @@ public class VideoController {
 	
 	private static final AtomicLong currentId = new AtomicLong(0L);
 	private Map<Long,Video> videos = new HashMap<Long, Video>();
+	private VideoFileManager videoDataMgr;
 
 	@RequestMapping(value=VideoSvcApi.VIDEO_SVC_PATH, method=RequestMethod.GET)
 	public @ResponseBody Collection<Video> getVideoList(){
@@ -76,18 +78,36 @@ public class VideoController {
 	}	
 	
 	@RequestMapping(value=VideoSvcApi.VIDEO_DATA_PATH, method=RequestMethod.POST)
-	public VideoStatus setVideoData(
-			@RequestParam(VideoSvcApi.ID_PARAMETER) long id, 
-			@RequestParam(VideoSvcApi.DATA_PARAMETER) TypedFile videoData) {
-		return new VideoStatus(VideoStatus.VideoState.READY); // ALDBG TODO
+	public @ResponseBody VideoStatus setVideoData(
+			@PathVariable (VideoSvcApi.ID_PARAMETER) long id, 
+			@RequestParam (VideoSvcApi.DATA_PARAMETER) MultipartFile videoData,
+			HttpServletResponse response) throws IOException {
+		videoDataMgr = VideoFileManager.get();
+		if (videos.containsKey(id)) {
+			Video v = videos.get(id);
+			saveSomeVideo(v, videoData);
+		} else {
+			response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+		}
+
+		return new VideoStatus(VideoStatus.VideoState.READY);
 	}
 	
 	@RequestMapping(value=VideoSvcApi.VIDEO_DATA_PATH, method=RequestMethod.GET)
-	Response getData(@RequestParam(VideoSvcApi.ID_PARAMETER) long id) {
-		return null; // ALDBG TODO
+	public void getData(
+			@PathVariable(VideoSvcApi.ID_PARAMETER) long id, 
+			HttpServletResponse response) throws IOException {
+		if (videos.containsKey(id)) {
+			Video v = videos.get(id);
+			if (videoDataMgr.hasVideoData(v)) {
+				serveSomeVideo(v, response);
+				return;
+			}
+		}
+		response.setStatus(HttpServletResponse.SC_NOT_FOUND);		
 	}
 	
-  	public Video save(Video entity) {
+  	private Video save(Video entity) {
 		checkAndSetId(entity);
 		entity.setDataUrl(getDataUrl(entity.getId()));
 		videos.put(entity.getId(), entity);
@@ -113,5 +133,17 @@ public class VideoController {
 	      + ((request.getServerPort() != 80) ? ":"+request.getServerPort() : "");
 	   return base;
 	}
+
+    // You would need some Controller method to call this...
+  	private void saveSomeVideo(Video v, MultipartFile videoData) throws IOException {
+  	     videoDataMgr.saveVideoData(v, videoData.getInputStream());
+  	}
+  	
+  	private void serveSomeVideo(Video v, HttpServletResponse response) throws IOException {
+  	     // Of course, you would need to send some headers, etc. to the
+  	     // client too!
+  	     //  ...
+  	     videoDataMgr.copyVideoData(v, response.getOutputStream());
+  	}
 
 }
